41a54605d2a3d6d0ca8895e0d5cc2961100e31ce
hskhedekar <hskhedekar@gmail.com>
2020-09-04 15:32:53 +0530

    Intial Commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+;(function () {
+	
+	'use strict';
+
+	var owlCarousel = function(){
+
+        new WOW().init();
+
+        $('.owl-carousel').owlCarousel({
+            items : 4,
+            loop  : true,
+            margin : 170,
+            center : true,
+            smartSpeed :900,
+            nav:true,
+            navText: [
+                "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
+                "<i class='fa carousel-right-arrow fa-chevron-right'></i>"
+            ],responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                    nav:true
+                },
+                600:{
+                    items:1,
+                    nav:true,
+                    margin : 120,
+                },
+                1000:{
+                    items:3,
+                    nav:true,
+                    loop:true,
+                    autoplay: true,
+                    autoplayTimeout: 1500,
+                    navText: [
+                        "<i class='fa carousel-left-arrow fa-chevron-left'></i>",
+                        "<i class='fa carousel-right-arrow fa-chevron-right'></i>"
+                    ],
+                }
+            }
+        });
+
+	};
+
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 'slow');
+        return this; // for chaining...
+    }
+
+	$(function(){
+		owlCarousel();
+	});
+
+
+}());
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
